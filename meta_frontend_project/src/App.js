b3570346c9ac29766ae9425b3b103d5d84bcceb7
Barchynai Kochkonbai kyzy <barchynaikochkonbaikyzy@MacBook-Air-Barchynai.local>
2025-06-20 08:04:29 +0600

    added routes

--- meta_frontend_project/src/App.js
+++ meta_frontend_project/src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import Nav from './components/Nav';
 import Homepage from './components/Homepage';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Specials from './components/Specials';
 
 
 
 function App() {
   return (
-    <div>
+    <Router>
       <header><Nav/></header>
       <main><Homepage/></main>
       <footer><Footer/></footer>
-    </div>
+      <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/about' elemet={<Chicago/>}/>
+      <Route path='/menu' element={<Specials/>}/>
+      <Route path='/reservation' element={<BookingPage/>}/>
+      <Route path='/order-online' element={<CallToAction/>}/>
+      </Routes>
+    </Router>
      
    
   );
--- meta_frontend_project/src/components/Nav.js
+++ meta_frontend_project/src/components/Nav.js
@@ -1,23 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import { Link } from 'react-router-dom';
 
 import headerLogo from '../images/header_logo.png'
 function Nav (){
   const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef(null);
 
   const toggleMenu = () =>{
     setIsOpen(!isOpen);
   };
 
+  useEffect(()=>{
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)){
+        setIsOpen(false)
+      }
+    }
+  document.addEventListener('mousedown', handleClickOutside)
+  return () =>{
+  document.removeEventListener('mousedown', handleClickOutside);
+  }
+  }, []);
+
   return (
-    <nav className="nav-bar">
+    <nav className="nav-bar" ref={navRef}>
       <img src={headerLogo} alt="Logo" className="header-logo" />
       <div className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#services">Menu</a>
-        <a href="#contact">Reservation</a>
-        <a href="#contact">Order Online</a>
-        <a href="#contact">Login</a>
+        <Link to='/' onClick={()=>setIsOpen(false)}>Home</Link>
+        <Link to='/about' onClick={()=>setIsOpen(false)}>About</Link>
+        <Link to='/menu' onClick={()=>setIsOpen(false)}>Menu</Link>
+        <Link to='/reservation' onClick={()=>setIsOpen(false)}>Reservation</Link>
+        <Link to='/order-online' onClick={()=>setIsOpen(false)}>Order Online</Link>
       </div>
 
         {/* humberger icon */}
